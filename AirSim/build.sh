da182fb67594870c8bd2565f475855701e2ee055
davidoort <davidoort@hotmail.com>
2020-05-07 19:20:14 +0200

    commented out mavlink references in build script

--- AirSim/build.sh
+++ AirSim/build.sh
@@ -73,13 +73,13 @@ mkdir -p AirLib/lib/x64/Debug
 mkdir -p AirLib/deps/rpclib/lib
 mkdir -p AirLib/deps/MavLinkCom/lib
 cp $build_dir/output/lib/libAirLib.a AirLib/lib
-cp $build_dir/output/lib/libMavLinkCom.a AirLib/deps/MavLinkCom/lib
+#cp $build_dir/output/lib/libMavLinkCom.a AirLib/deps/MavLinkCom/lib
 cp $build_dir/output/lib/librpc.a AirLib/deps/rpclib/lib/librpc.a
 
 # Update AirLib/lib, AirLib/deps, Plugins folders with new binaries
 rsync -a --delete $build_dir/output/lib/ AirLib/lib/x64/Debug
 rsync -a --delete external/rpclib/rpclib-2.2.1/include AirLib/deps/rpclib
-rsync -a --delete MavLinkCom/include AirLib/deps/MavLinkCom
+#rsync -a --delete MavLinkCom/include AirLib/deps/MavLinkCom
 rsync -a --delete AirLib ../UE4Project/Plugins/AirSim/Source
 
 set +x
