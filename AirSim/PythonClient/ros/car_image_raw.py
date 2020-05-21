ff533cca03feb29bd6d276c41d86c51ee0b5815d
sijmenhuizenga <sijmenhuizenga@gmail.com>
2020-05-21 16:06:11 +0200

    Make it run as fast as possible

--- AirSim/PythonClient/ros/car_image_raw.py
+++ AirSim/PythonClient/ros/car_image_raw.py
@@ -4,7 +4,7 @@
 
 import setup_path 
 import airsim
-
+import time
 import rospy
 
 # ROS Image message
@@ -13,16 +13,18 @@ from sensor_msgs.msg import Image
 def airpub():
     pub = rospy.Publisher("airsim/image_raw", Image, queue_size=1)
     rospy.init_node('image_raw', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-
-    # connect to the AirSim simulator 
+    rate = rospy.Rate(15)# connect to the AirSim simulator 
     client = airsim.CarClient()
     client.confirmConnection()
 
     while not rospy.is_shutdown():
          # get camera images from the car
+        start = time.time()
         responses = client.simGetImages([
-            airsim.ImageRequest("1", airsim.ImageType.Scene, False, False)])  #scene vision image in uncompressed RGB array
+            airsim.ImageRequest("front_center_custom", airsim.ImageType.Scene, False, False),
+            airsim.ImageRequest("front_left_custom", airsim.ImageType.Scene, False, False)
+            ])
+        print(time.time()-start)
 
         for response in responses:
             img_rgb_string = response.image_data_uint8
@@ -32,18 +34,17 @@ def airpub():
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = "frameId"
         msg.encoding = "rgb8"
-        msg.height = 360  # resolution should match values in settings.json 
+        msg.height = 480  # resolution should match values in settings.json 
         msg.width = 640
         msg.data = img_rgb_string
         msg.is_bigendian = 0
         msg.step = msg.width * 3
 
         # log time and size of published image
-        rospy.loginfo(len(response.image_data_uint8))
         # publish image message
         pub.publish(msg)
         # sleep until next cycle
-        rate.sleep()
+        #rate.sleep()
 
 
 if __name__ == '__main__':
