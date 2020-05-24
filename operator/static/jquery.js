7ccd0c6bd55f520a6a7c9aa3d24d108d1d44901e
mvanlobensels <mavanlobensels@gmail.com>
2020-05-24 21:33:35 +0200

    access token fix

--- operator/static/jquery.js
+++ operator/static/jquery.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     const logs = [];
     pollServer();
-
+    
     // Poll logs from server every 5 seconds
     function pollServer() {
         $.ajax('logs', {
@@ -18,27 +18,27 @@ $(document).ready(function() {
         });
         setTimeout(pollServer, 5000);
     }
-
+    
     // Start button handler
     $('#start').click(() => {
-        const accessToken = $('access-token').val();
-        if (accessToken === undefined) {
+        const accessToken = $('#access-token').val();
+        if (accessToken === '') {
             alert('Give an access token.');
             return
         }
-
+    
         const selectedTeam = $("input:radio[name ='team-select']:checked").val();
         if (selectedTeam === undefined) {
             alert('Select a team.');
             return
         }
-
+    
         const selectedMission = $("input:radio[name ='mission-select']:checked").val();
         if (selectedMission === undefined) {
             alert('Select a mission.');
             return
         }
-
+    
         $.ajax('mission/start', {
             data: JSON.stringify({id: selectedTeam, mission: selectedMission, access_token: accessToken}),
             contentType: 'application/json',
@@ -52,15 +52,15 @@ $(document).ready(function() {
             }
         });
     });
-
+    
     // Stop button handler
     $('#stop').click(() => {
-        const accessToken = $('access-token').val();
-        if (accessToken === undefined) {
+        const accessToken = $('#access-token').val();
+        if (accessToken === '') {
             alert('Give an access token.');
             return
         }
-
+    
         $.ajax('mission/stop', {
             data: JSON.stringify({access_token: accessToken}),
             contentType: 'application/json',
@@ -74,15 +74,15 @@ $(document).ready(function() {
             }
         });     
     });
-
+    
     // Reset button handler
     $('#reset').click(function() {
-        const accessToken = $('access-token').val();
-        if (accessToken === undefined) {
+        const accessToken = $('#access-token').val();
+        if (accessToken === '') {
             alert('Give an access token.');
             return
         }
-
+    
         $.ajax('mission/reset', {
             data: JSON.stringify({access_token: accessToken}),
             contentType: 'application/json',
