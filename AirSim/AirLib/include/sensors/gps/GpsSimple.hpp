340a5c79a67d92ed7ab96b65154b973bc16877e6
skimslozo <ozolsmiks@gmail.com>
2020-05-30 08:17:10 +0000

    implemented constant zero-velocity offset

--- AirSim/AirLib/include/sensors/gps/GpsSimple.hpp
+++ AirSim/AirLib/include/sensors/gps/GpsSimple.hpp
@@ -32,6 +32,8 @@ public: //methods
         //initialize filters
         eph_filter.initialize(params_.eph_time_constant, params_.eph_final, params_.eph_initial); //starting dilution set to 100 which we will reduce over time to targeted 0.3f, with 45% accuracy within 100 updates, each update occurring at 0.2s interval
         epv_filter.initialize(params_.epv_time_constant, params_.epv_final, params_.epv_initial);
+        //get zero velocity offset
+        zero_vel_offset=getZeroVelOffset(params_.eph_final, params_.epv_final);
     }
 
     //*** Start: UpdatableState implementation ***//
@@ -66,7 +68,7 @@ public: //methods
     //*** End: UpdatableState implementation ***//
 
     virtual ~GpsSimple() = default;
-
+    //generate Gaussian noise 
     double getGaussianNoise(const double mean, const double var)
     {
         std::normal_distribution<double> distribution(mean, var);
@@ -74,7 +76,7 @@ public: //methods
         std::default_random_engine       generator(seed);
         return distribution(generator);
     }
-
+    //Generate noisy geo_point for moving vehicle
     msr::airlib::GeoPoint generateErrors(msr::airlib::GeoPoint geo_point, real_T eph, real_T epv)
     {
         HomeGeoPoint geo_point_in = HomeGeoPoint(geo_point);
@@ -89,11 +91,12 @@ public: //methods
         return geo_point_out;
     }
 
-    // void getZeroVelOffset()
-    // {   float var_h = pow(G, 2)/18;
-    //     float var_v = pow(epv, 2)/9;
-    //     return EarthUtils::nedToGeodetic(msr::airlib::Vector3r(getGaussianNoise(0, var_h), getGaussianNoise(0,var_h), getGaussianNoise(0, var_v)), geo_point_in);
-    // }
+    //Create a constant offset to add to the geo_point when vehicle at a standstill
+    msr::airlib::Vector3r getZeroVelOffset(real_T eph_final, real_T epv_final)
+    {   float var_h = pow(eph_final, 2)/18;
+        float var_v = pow(epv_final, 2)/9;
+        return msr::airlib::Vector3r(getGaussianNoise(0, var_h), getGaussianNoise(0, var_h), getGaussianNoise(0, var_v));
+    }
 
 private:
     void addOutputToDelayLine(real_T eph, real_T epv)
@@ -104,11 +107,12 @@ private:
         //GNSS
         output.gnss.time_utc = static_cast<uint64_t>(clock()->nowNanos() / 1.0E3);
         output.gnss.velocity = ground_truth.kinematics->twist.linear;
+        //add noise to the GPS position measurements depending on the velocity of the vehicle
         if ((gpsnoise) && (sqrt(pow(output.gnss.velocity.x(), 2) + pow(output.gnss.velocity.y(), 2) + pow(output.gnss.velocity.z(), 2))>0.5)) {
             output.gnss.geo_point=generateErrors(ground_truth.environment->getState().geo_point, eph, epv);
         }
         else {
-            output.gnss.geo_point=ground_truth.environment->getState().geo_point;
+            output.gnss.geo_point=EarthUtils::nedToGeodetic(zero_vel_offset, ground_truth.environment->getState().geo_point);
         }
         output.gnss.eph = eph;
         output.is_valid = true;
@@ -128,6 +132,7 @@ private:
 private:
     typedef std::normal_distribution<> NormalDistribution;
     bool gpsnoise=true;
+    msr::airlib::Vector3r zero_vel_offset;
     GpsSimpleParams params_;
     FirstOrderFilter<real_T> eph_filter, epv_filter;
     FrequencyLimiter freq_limiter_;
