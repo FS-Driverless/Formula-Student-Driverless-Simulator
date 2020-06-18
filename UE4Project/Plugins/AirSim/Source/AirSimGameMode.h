93d48db4c22423390c99307149981d470daccc2d
Sijmen Huizenga <sijmenhuizenga@gmail.com>
2020-06-18 20:53:00 +0200

    Support multiplayer without crashing. Removed HUD camera preview and recording abilities because couldn't get them to work in multiplayer.

--- UE4Project/Plugins/AirSim/Source/AirSimGameMode.h
+++ UE4Project/Plugins/AirSim/Source/AirSimGameMode.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
 #include "GameFramework/GameUserSettings.h"
+#include "SimMode/SimModeBase.h"
 #include "AirSimGameMode.generated.h"
 
 /**
@@ -17,10 +18,23 @@ public:
 	GENERATED_BODY()
     
     virtual void StartPlay() override;
+    virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
     
     AAirSimGameMode(const FObjectInitializer& ObjectInitializer);
+
+    UPROPERTY() ASimModeBase* simmode;
+
     
-//private:
-    //UGameUserSettings* GetGameUserSettings();
+private:
+    void initializeSettings();
+    bool getSettingsText(std::string& settingsText);
+    bool getSettingsTextFromCommandLine(std::string& settingsText);
+    bool readSettingsTextFromFile(FString fileName, std::string& settingsText);
+    std::string getSimModeFromUser();
+
+    void setUnrealEngineSettings();
+    void createSimMode();
+
 
 };
