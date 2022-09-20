832d02a418a61ad46c7e863dfca95158fd0c790f
Wouter Heerwegh <wouter.heerwegh@gmail.com>
2022-09-20 16:01:48 +0200

    Docs version changes

--- docker/download_FSDSSimulator_binary.sh
+++ docker/download_FSDSSimulator_binary.sh
@@ -1,5 +1,5 @@
 # Shell script to download Simulator bins from github; For custom versions of simulator path has to be updated
 sudo apt-get install unzip
-wget -c https://github.com/FS-Driverless/Formula-Student-Driverless-Simulator/releases/download/v2.1.0/fsds-v2.1.0-linux.zip
-unzip fsds-v2.1.0-linux.zip
-rm fsds-v2.1.0-linux.zip
+wget -c https://github.com/FS-Driverless/Formula-Student-Driverless-Simulator/releases/download/v2.2.0/fsds-v2.2.0-linux.zip
+unzip fsds-v2.2.0-linux.zip
+rm fsds-v2.2.0-linux.zip
